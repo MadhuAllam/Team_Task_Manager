7bff91d28f885e197d10a2af1cd05840f9f68cca
Madhu Allam <madhuallam32@gmail.com>
2026-05-03 10:25:46 +0530

    Change route handler to use '*splat' for production

--- server/server.js
+++ server/server.js
@@ -67,7 +67,7 @@ const path = require('path');
 // Serve frontend in production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
-  app.get('(.*)', (req, res) => {
+  app.get('*splat', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
   });
 } else {
